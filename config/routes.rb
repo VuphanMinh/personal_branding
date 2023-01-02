afb937276d5d3b2e27a58bc8def7b09dc50c2d28
VuphanMinh <phanminhvuit@gmail.com>
2023-01-02 16:22:54 +0700

    setup_env admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    root 'dashboard#index'
+
+    # devise_scope :user do
+    #   get '/login', to: 'sessions#new', as: :login
+    #   post '/login', to: 'sessions#create'
+    # end
+  end
 end
